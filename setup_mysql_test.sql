d7b4347ef58148452957624d6705c905c6fa11a2
simret <simruineb@gmail.com>
2022-10-11 18:59:42 +0300

    sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,14 +3,11 @@
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
 -- Create user
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
-           IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test' @ 'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 -- Grant permissions on hbnb_test_db
-GRANT ALL PRIVILEGES ON hbnb_test_db.*
-   TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test' @ 'localhost';
 
 -- Grant permission on performance_schema
-GRANT SELECT ON performance_schema.*
-   TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test' @ 'localhost';
 FLUSH PRIVILEGES;
