815c5d51b35ccc8f15e987f7f73a9cbac99d55e6
simret <simruineb@gmail.com>
2022-10-11 19:02:01 +0300

    sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -10,4 +10,3 @@ GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test' @ 'localhost';
 
 -- Grant permission on performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_test' @ 'localhost';
-FLUSH PRIVILEGES;
