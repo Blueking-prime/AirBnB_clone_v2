d13b0f90cb7a7beced57f6191a5b0f700799b7b5
simret <simruineb@gmail.com>
2022-10-11 11:30:33 +0300

    Amenity place relationship

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from sqlalchemy import Column, String
+from email.policy import default
+from sqlalchemy import Column, String, Integer, ForeignKey, Table
+from sqlalchemy.orm import relationship
+import os
+from models.place import place_amenity
 from models import stor_type
 from models.base_model import Base, BaseModel
 
@@ -10,6 +14,7 @@ class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
 
     if stor_type == 'db':
-        name = Column(String(128), nullable=False)
+        name = Column(String(128), nullable=False, default="")
+        place_amenities  = relationship("place", secondary=place_amenity)
     else:
         name = ''
