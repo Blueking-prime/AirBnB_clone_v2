9ca5fe1f37f9c0e12e7f8675d413652e7e68465a
simret <simruineb@gmail.com>
2022-10-12 08:55:13 +0300

    Amenty and place

--- models/amenity.py
+++ models/amenity.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""State Module for HBNB project."""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, ForeignKey, Table
-from sqlalchemy.orm import relationship
-import os
-from models.place import place_amenity
+from sqlalchemy import Column, String
+import models
 
 
 class Amenity(BaseModel, Base):
-    '''
-        Implementation for the Amenities.
-    '''
-    __tablename__ = 'amenities'
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False, default="")
-        place_amenities = relationship(
-            "Place", secondary=place_amenity)
+    """Class Representing amenities."""
+
+    if models.storage_t == 'db':
+        from models.place import place_amenity
+        __tablename__ = "amenities"
+        name = Column(String(128), nullable=False)
     else:
         name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize Amenity."""
+        super().__init__(*args, **kwargs)
