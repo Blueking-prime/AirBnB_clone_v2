284258c7c9d3864bd0c45c3f2a5088eb0245ad1d
simret <simruineb@gmail.com>
2022-10-15 22:28:36 +0300

    Place amentiy relationship

--- models/amenity.py
+++ models/amenity.py
@@ -5,15 +5,11 @@ from models.base_model import BaseModel
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
-from os import getenv
 
 
 class Amenity(BaseModel, Base):
     """Represents an Amenity for a MySQL database."""
     __tablename__ = "amenities"
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable=False)
-        place_amenities = relationship("Place", secondary="place_amenity",
-                                       viewonly=False)
-    else:
-        name = ""
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   backref="amenity")
